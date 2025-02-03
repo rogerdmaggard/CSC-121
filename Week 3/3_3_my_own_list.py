c1c1437fe9c7188886d723d1a5cd251125058d79
rogerdmaggard <rogerdeanmaggard@gmail.com>
2025-02-02 19:11:16 -0500

    CSC-121 Week 3 Assignments

---
+++ Week 3/3_3_my_own_list.py
@@ -0,0 +1,11 @@
+sportsCars = ['porche', 'lambo', 'ferrari', 'mercedes', 'bmw']
+message1 = f"My first choice for a sports car is a {sportsCars[0].title()}."
+print(message1)
+message2 = f"My second choice for a sports car is a {sportsCars[1].title()}."
+print(message2)
+message3 = f"My third choice for a sports car is a {sportsCars[2].title()}."
+print(message3)
+message4 = f"My fourth choice for a sports car is a {sportsCars[3].title()}."
+print(message4)
+message5 = f"My fifth choice for a sports car is a {sportsCars[4].upper()}."
+print(message5)
---
+++ Week 3/3_4_guest_list.py
@@ -0,0 +1,7 @@
+guests = ['eddie van halen', 'david lee roth', 'albert einstein']
+message1 = f"Dear {guests.pop(0).title()}, You are cordially invited to dinner with me."
+print(message1)
+message2 = f"Dear {guests.pop(0).title()}, You are cordially invited to dinner with me."
+print(message2)
+message3 = f"Dear {guests.pop(0).title()}, You are cordially invited to dinner with me."
+print(message3)
